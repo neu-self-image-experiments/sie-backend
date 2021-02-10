76d0d4d8f3c55046855cfad425ce2be4746b954b
QA Hoang <qahoang317@gmail.com>
2021-02-09 23:14:07 -0800

    Fixed main.py to conform with black linter

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,5 +1,6 @@
 from flask import escape
 
+
 def hello_http(request):
     """HTTP Cloud Function.
     Args:
@@ -13,10 +14,10 @@ def hello_http(request):
     request_json = request.get_json(silent=True)
     request_args = request.args
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
+    if request_json and "name" in request_json:
+        name = request_json["name"]
+    elif request_args and "name" in request_args:
+        name = request_args["name"]
     else:
-        name = 'World'
-    return 'Hello {}!'.format(escape(name))
+        name = "World"
+    return "Hello {}!".format(escape(name))
