334d79a1af2546faae0ce448072925a0a864bda1
liuhantao9 <liuhantao9@gmail.com>
2021-02-14 15:14:00 -0800

    fixing typo

--- image-processing/main.py
+++ image-processing/main.py
@@ -64,8 +64,8 @@ def download_image(bucket_name, source_blob_name, destination_file_name):
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(destination_file_name)
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
+def upload_processeed_images(bucket_name, source_file_name, destination_blob_name):
+    """Uploads image files to the bucket."""
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
