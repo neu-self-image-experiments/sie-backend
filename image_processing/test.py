59a44d21ad9ee4363cee12165f9fd9db9442d118
liuhantao9 <liuhantao9@gmail.com>
2021-02-23 16:07:05 -0800

    fixing issues

--- image_processing/test.py
+++ image_processing/test.py
@@ -2,8 +2,8 @@ import os
 import main
 from google.cloud import storage
 
+
 def test_upload_processed_images(request):
-    request_json = request.get_json(silent=True)
     request_args = request.args
 
     # try:
@@ -13,19 +13,16 @@ def test_upload_processed_images(request):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
 
-    print(bucket)
-
     main.upload_processed_images(bucket_name, source_file_folder)
 
     for file_name in os.listdir(source_file_folder):
         blob = bucket.blob(file_name)
-        print(file_name)
-        print(blob.exist())
         if not blob.exist():
             return "Test fail, upload unsuccessful"
     
     return "Test passed"
     # except Exception:
-    #     return "Exception is thrown, check the parameter that are passed into the function"
+    #     return "Exception is thrown, check 
+    #       the parameter that are passed into the function"
     # finally:
     #     return "Test passed"
