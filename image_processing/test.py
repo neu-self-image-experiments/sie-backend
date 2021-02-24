e9c8e59e485f497bba64f52bb6dc1c6ca8aded31
liuhantao9 <liuhantao9@gmail.com>
2021-02-23 19:24:49 -0800

    Finish Testing

--- image_processing/test.py
+++ image_processing/test.py
@@ -4,20 +4,32 @@ from google.cloud import storage
 
 
 def test_upload_processed_images(request):
-    request_args = request.args
+    """Unit test for the function upload_processed_images.
+    Args:
+        request = "HTTP request"
 
-    # try:
+    Returns:
+        String if all assertion passed;
+    """
+    request_args = request.args
     bucket_name = request_args["bucket_name"]
     source_file_folder = request_args["source_file_folder"]
 
+    # Create connection to the storage
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
 
     main.upload_processed_images(bucket_name, source_file_folder)
 
+    # Iterate through the files inside the folder
     for file_name in os.listdir(source_file_folder):
         blob = bucket.blob(file_name)
-        if not blob.exist():
-            return "Test fail, upload unsuccessful"
+        # Aessert if the blob exist in the bucket
+        assert storage.Blob(bucket=bucket, name=file_name).exists(storage_client) == True
+        # You can use print to see if the assertion is correct
 
+    # Make a falsy test to ensure that it returns false
+    assert storage.Blob(bucket=bucket, name="Falsy_test").exists(storage_client) == False
+
+    # If all assertions passed, then return test pass
     return "Test passed"
