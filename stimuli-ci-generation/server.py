76f3f6cfaf6ca84583a03bc2ffba777e06dc480a
xuzhouyuan <xuzhouyuan@gmail.com>
2021-02-25 10:25:48 -0800

    Update server.py

--- stimuli-ci-generation/server.py
+++ stimuli-ci-generation/server.py
@@ -105,9 +105,7 @@ def generate_ci():
 
 
 if __name__ == "__main__":
-    app.debug = True
     app.run(
-        debug=True,
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8080)),
         threaded=True,
