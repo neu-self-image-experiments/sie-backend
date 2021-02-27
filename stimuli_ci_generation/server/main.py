2c34e4647a3f4ce3045665c5dc7fae599b6276ed
QA Hoang <qahoang317@gmail.com>
2021-02-27 11:44:25 -0800

    implemented pub/sub communication between cloud run and cloud storage

--- stimuli_ci_generation/server/main.py
+++ stimuli_ci_generation/server/main.py
@@ -2,27 +2,60 @@ from flask import Flask
 from flask import request
 
 import os
+import base64
+import json
 
 app = Flask(__name__)
 
 
 # [START eventarc_gcs_handler]
 @app.route("/", methods=["POST"])
-def storage_trigger_event():
+def index():
     """
-    Listens for storage event triggers and run image processing
+    Listens for message from storage trigger and run image processing
     """
 
-    bucket = request.headers.get("ce-subject")
-    print(f"Detected change in Cloud Storage bucket: {bucket}")
-    # TODO(qhoang)
-    # extract file_name and participant_id
-    # download image file
-    # run image processing from stimuli_ci.py
+    envelope = request.get_json()
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
 
-    return (f"Detected change in Cloud Storage bucket: {bucket}", 200)
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
 
+    # Decode the Pub/Sub message.
+    pubsub_message = envelope["message"]
 
+    name = "World"
+    if isinstance(pubsub_message, dict) and "data" in pubsub_message:
+        try:
+            data = json.loads(base64.b64decode(pubsub_message["data"]).decode())
+
+        except Exception as e:
+            msg = (
+                "Invalid Pub/Sub message: "
+                "data property is not valid base64 encoded JSON"
+            )
+            print(f"error: {e}")
+            return f"Bad Request: {msg}", 400
+
+              # Validate the message is a Cloud Storage event.
+        if not data["name"] or not data["bucket"]:
+            msg = (
+                "Invalid Cloud Storage notification: "
+                "expected name and bucket properties"
+            )
+            print(f"error: {msg}")
+            return f"Bad Request: {msg}", 400
+
+        print(data)
+        print(data["name"])
+        print(data["bucket"])    
+
+    return ("", 204)
 # [END eventarc_gcs_handler]
 
 
