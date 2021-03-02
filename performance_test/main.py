e8030a32a1f606eb48970a19203ada61c6913380
liuhantao9 <liuhantao9@gmail.com>
2021-03-01 17:03:46 -0800

    adding pieces

--- performance_test/main.py
+++ performance_test/main.py
@@ -4,6 +4,7 @@ import queue
 from concurrent.futures import ThreadPoolExecutor
 
 SLEEP_TIME = 2
+FILE_DIR = "./test_data/"
 
 
 def performance_test(num_thread):
@@ -15,8 +16,15 @@ def performance_test(num_thread):
 
         for i in range(num_thread):
             t = thread(i, SLEEP_TIME, start_queue, end_queue)
-            executor.submit(t.run)
+            executor.submit(t.run, (FILE_DIR, str(i) + ".jpg"))
 
-    # TODO iterate over two queues and find the smallest
-    # start time and largest end time, to calculate the
-    # throughput
+    min_start_time = start_queue.get()
+    max_end_time = end_queue.get()
+
+    while start_queue.empty() is False:
+        min_start_time = min(min_start_time, start_queue.get())
+    
+    while end_queue.empty() is False:
+        max_end_time = max(max_end_time, end_queue.get())
+    
+    print("The resulting throughput of the program is {}".format(float((max_end_time - min_start_time) / num_thread)))
