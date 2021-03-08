d11c8fdc4e9d70c5c7208c1c4098fc6bd14bb6aa
QA Hoang <qahoang317@gmail.com>
2021-03-08 14:03:00 -0800

    id is a bad variable name -> changed to identifier

--- stimuli_ci_generation/src/server/stimuli_ci.py
+++ stimuli_ci_generation/src/server/stimuli_ci.py
@@ -11,24 +11,24 @@ import gcp_config
 USER_SELECTION = "user_selection.csv"
 
 
-def generate_stimuli(id, file_name):
+def generate_stimuli(identifier, file_name):
     """
     Run stimuli generation and save processed images for experiment
     Args:
-        id: participant_id-experiment_id extracted from img filename
+        identifier: participant_id-experiment_id extracted from img filename
 
     Returns:
         None
     """
     downloaded_path = download_file(
         gcp_config.MASKED_IMG_BUCKET,
-        f"{id}/{file_name}",
-        f"{mkdir(id)}/{file_name}",
+        f"{identifier}/{file_name}",
+        f"{mkdir(identifier)}/{file_name}",
     )
     print("downloaded_to:", downloaded_path)
 
-    output_dir = mkdir(id)
-    stimuli_dir = mkdir(id, "stimuli")
+    output_dir = mkdir(identifier)
+    stimuli_dir = mkdir(identifier, "stimuli")
     r_script_path = f"{os.getcwd()}/rscript/generate_stimuli.R"
 
     try:
@@ -36,28 +36,28 @@ def generate_stimuli(id, file_name):
             ["Rscript", "--vanilla", r_script_path, output_dir], shell=False
         )
         print("Finished running generate_stimuli.R")
-        upload_dir(gcp_config.STIMULI_IMG_BUCKET, stimuli_dir, id)
+        upload_dir(gcp_config.STIMULI_IMG_BUCKET, stimuli_dir, identifier)
     except subprocess.CalledProcessError as err:
         print("Error running generate_stimuli.R", err)
         raise err
 
 
-def generate_ci(id, file_name):
+def generate_ci(identifier, file_name):
     """
     Run ci and upload results to cloud storage
     Args:
-        id: participant_id-experiment_id extracted from img filename
+        identifier: participant_id-experiment_id extracted from img filename
 
     Returns:
         None
     """
-    ws_dir = mkdir(id)
-    ci_dir = mkdir(id, "ci")
+    ws_dir = mkdir(identifier)
+    ci_dir = mkdir(identifier, "ci")
     r_script_path = f"{os.getcwd()}/rscript/generate_ci.R"
 
     if not os.path.exists(f"{ws_dir}/stimuli"):
-        mkdir(id, "stimuli")
-        download_dir(gcp_config.STIMULI_IMG_BUCKET, id, f"{ws_dir}/stimuli")
+        mkdir(identifier, "stimuli")
+        download_dir(gcp_config.STIMULI_IMG_BUCKET, identifier, f"{ws_dir}/stimuli")
         print(f"downloaded stimuli images to {ws_dir}/stimuli")
 
     download_file(
@@ -70,7 +70,7 @@ def generate_ci(id, file_name):
     try:
         subprocess.check_call(["Rscript", r_script_path, ws_dir], shell=False)
         print("Finished running generate_ci.R")
-        upload_dir(gcp_config.CI_IMG_BUCKET, ci_dir, id)
+        upload_dir(gcp_config.CI_IMG_BUCKET, ci_dir, identifier)
     except subprocess.CalledProcessError as err:
         print("Error running generate_ci.R", err)
         raise err
