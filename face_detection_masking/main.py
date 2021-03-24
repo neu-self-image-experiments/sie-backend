9db76ecf644d599726a6cf6f7ecabc3d6a25dc8b
Jaynil Patel <jaynilbvb@gmail.com>
2021-03-24 02:40:04 -0400

    SIE-74 Removed deprecated function

--- face_detection_masking/main.py
+++ face_detection_masking/main.py
@@ -267,42 +267,6 @@ def resize(image):
         return None
 
 
-def trigger_event(event, context):
-    """
-    Background Cloud Function to be triggered by Cloud Storage.
-    This generic function logs relevant data when a file is changed.
-
-    Args:
-        event (dict):  The dictionary with data specific to this type of event.
-                        The `data` field contains a description of the event in
-                        the Cloud Storage `object` format described here:
-                        https://cloud.google.com/storage/docs/json_api/v1/objects#resource
-        context (google.cloud.functions.Context): Metadata of triggering event.
-    Returns:
-        None; the output is written to Stackdriver Logging
-    """
-
-    bucket_name = event["bucket"]
-    source_img = event["name"]
-    tmp_download_path = f"/tmp/{source_img}"
-
-    try:
-        download_image(bucket_name, source_img, tmp_download_path)
-        print(f"Image {source_img} downloaded to {tmp_download_path}")
-        # currenlty this makes sure there's one person
-        # in the frame and prints a few other details
-        face_detection(tmp_download_path)
-        # TODO(jerry) call R script with features returned from Abi's part
-        # TODO(hantao) put processed images to `sie-processed-images` bucket
-    except Exception:
-        pass
-    finally:
-        if os.path.isfile(tmp_download_path):
-            os.remove(tmp_download_path)
-        else:
-            print("Error: %s file not found" % tmp_download_path)
-
-
 def download_image(bucket_name, source_blob_name, destination_file_name):
     """Downloads the specified image from cloud storage
     Args:
