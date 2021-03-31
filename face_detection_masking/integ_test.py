e8ecef0d34e0fd6a4190328907bc2071947cbeae
M Z <man@ms-macbook-pro.lan>
2021-03-30 21:36:51 -0700

    Add description for the issue I am facing when running integ_test.py

--- face_detection_masking/integ_test.py
+++ face_detection_masking/integ_test.py
@@ -1,4 +1,4 @@
-=import datetime
+import datetime
 import os
 import requests
 import subprocess
@@ -8,26 +8,45 @@ from google.cloud import storage
 from urllib3.util import Retry
 
 
-# My original plan to use "functions-framework" to launch "trigger_detect_and_mask" directly
-# in the test code, but I ketp getting errors when doing Session.post.
-# As an alternative, I run "functions-framework --target=trigger_detect_and_mask --port=9797 --signature-type=event"
-# first, and then in a new terminal run "pytest integ_test.py".
+# I am using the credentials of service account cs6510-sie-backend-testing@cs6510-spr2021.iam.gserviceaccount.com.
 
-# I am currently having issues with credentials. The one I am using cloud-vision@cs6510-spr2021.iam.gserviceaccount.com
-# doesn't seem to have permission to run the function, and no permission to access cloud storage.
+# Problems I am having right now:
+# 1. source venv/bin/activate
+# 2. Export the credentials of service account cs6510-sie-backend-testing@cs6510-spr2021.iam.gserviceaccount.com.
+# 3. Run the test using 'pytest integ_test.py', I got error like
+# requests.exceptions.ConnectionError: ('Connection aborted.', RemoteDisconnected('Remote end closed connection without response'))
+
+# Alternatively, I tried
+# 1. source venv/bin/activate
+# 2. Export the credentials of service account cs6510-sie-backend-testing@cs6510-spr2021.iam.gserviceaccount.com.
+# 3. Run "functions-framework --target=trigger_detect_and_mask --port=9898 --signature-type=event"
+# 4. Run "pytest integ_test.py" in another terminal (comment out the process part for functions-framework), I am still getting the same error.
+
+# However, I tried
+# 1. source venv/bin/activate
+# 2. Don't export the service account credentials
+# 3. Run "functions-framework --target=trigger_detect_and_mask --port=9898 --signature-type=event"
+# 4. In another terminal, export the service account credentials, and then run "pytest integ_test.py"
+# 5. The request did get to trigger_detect_and_mask (I could see debug logs showing up for request parameters), but I am getting
+# error in "results = face_detection(uri)"
+# google.auth.exceptions.DefaultCredentialsError: Could not automatically determine credentials.
+# Please set GOOGLE_APPLICATION_CREDENTIALS or explicitly create credentials and re-run the application. For more information, please see https://cloud.google.com/docs/authentication/getting-started
+
+
+# So, if I export the credentials when running functions-framework, requests from the integ test just could NOT hit
+# the localhost:9898. If I don't export the credentials, the requests could hit localhost:9898, but trigger_detect_and_mask
+# fails because of credentials could not be determined.
+# Currently blocked as I couldn't figure out the cause of the problem.
 
 def test_trigger_detect_and_mask():
-    # Bootstrap
     input_bucket_name = "integ_test_sie-raw-images"
-    output_bucket_name = "integ_test_sie-masked-images"
+    output_bucket_name = "sie-masked-images"
+
+    # The test image has already been uploaded to the input bucket
     image_name = "integ-test-human.jpeg"
 
-    # Create buckets
-    # set_up_bucket(input_bucket_name)
-    # set_up_bucket(output_bucket_name)
-
     # Each running framework instance needs a unique port
-    port = 9797
+    port = 9898
 
     create_timestamp = datetime.datetime.now().isoformat()
     storage_message = {
@@ -40,6 +59,7 @@ def test_trigger_detect_and_mask():
         }
     }
 
+    # TODO - comment out to run functions-framework separately
     # Run functions-framework instead manually
     # process = subprocess.Popen(
     #     [
@@ -65,7 +85,6 @@ def test_trigger_detect_and_mask():
     response = session.post(url, json=storage_message)
 
     assert response.status_code == 200
-    assert 'Face detected and masked successfully!' in response.text
 
     # Stop the functions framework process
     # process.kill()
@@ -76,13 +95,12 @@ def test_trigger_detect_and_mask():
     # assert 'Face detected and masked successfully!' in str(out)
 
     # # Validate images in the output bucket
-    # participant_id = os.path.splitext(image_name)[0]
-    # output_file_name = f"{participant_id}/{constants.PROCESSED_IMAGE}"
-    # assert validate_file_in_bucket(output_bucket_name, output_file_name)
-    #
-    # # Clean up buckets
-    # # clean_up_bucket(input_bucket_name)
-    # clean_up_bucket(output_bucket_name, participant_id)
+    participant_id = os.path.splitext(image_name)[0]
+    output_file_name = f"{participant_id}/{constants.PROCESSED_IMAGE}"
+    assert validate_file_in_bucket(output_bucket_name, output_file_name)
+
+    # Clean up buckets
+    clean_up_bucket(output_bucket_name, participant_id)
 
 
 def set_up_bucket(bucket_name):
